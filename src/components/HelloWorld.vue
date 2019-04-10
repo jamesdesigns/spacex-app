4d7cff52262736b58bdbb31c8d11954a7b362fba
James Hooper <jameshooperdesigns@gmail.com>
2019-04-10 15:07:30 -0600

    Add Breadcrumbs to the HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,10 @@
     text-xs-center
     wrap 
 >
-<div> {{ pokemon }} </div>
+<div> {{ info }} 
+
+    <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
+</div>
 </v-layout>
 </v-container>
 </template>
@@ -13,27 +16,45 @@ import axios from 'axios'
 import gql from 'graphql-tag'
 
 export default {
-  components: {
-    PokeCard
-  },
+  // components: {
+  //   PokeCard
+  // },
   data() {
     return {
       info: "",
-      pokemons: [],
+      // pokemons: [],
+       items: [
+    {
+      text: 'Home',
+      disabled: false,
+      href: 'breadcrumbs_dashboard'
+    },
+    {
+      text: 'Products',
+      disabled: false,
+      href: 'breadcrumbs_link_1'
+    },
+    {
+    text: 'Details',
+    disabled: true,
+    href: 'breadcrumbs_link_2'
+    }
+  ],
     }
   },
+   
   mounted () {
     // return axios
     // .get('https://pokeapi.co/api/v2/pokemon/1')
     // .then(response => (this.info = response))
   },
   apollo: {
-    pokemons: gql`query {
-      pokemons {
-        name
-        originalId
-      }
-    }`,
+    // pokemons: gql`query {
+    //   pokemons {
+    //     name
+    //     originalId
+    //   }
+    // }`,
    }
   };
   </script>
