4bd08dd6cf5f7abf647b6f6c69666e804855cb47
James Hooper <jameshooperdesigns@gmail.com>
2019-04-10 15:06:52 -0600

    Add search icon

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,22 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>SpaceX</span>
-        <span class="font-weight-light">Products</span>
+        <span class="font-weight-light"> Products</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
+      <!-- <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
         <span class="mr-2">Latest Release</span>
-      </v-btn>
+        
+      </v-btn> -->
+
+      <v-btn icon>
+            <v-icon>search</v-icon>
+          </v-btn>
+ 
     </v-toolbar>
 
     <v-content>
@@ -24,13 +30,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
-import PokeList from './components/PokeList'
+// import PokeList from './components/PokeList'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    PokeList
+    // PokeList
   },
   data () {
     return {
