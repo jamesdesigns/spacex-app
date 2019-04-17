c268e2b22d28f05ed11ad551778c6e69e575e4f4
James Hooper <jameshooperdesigns@gmail.com>
2019-04-17 13:19:34 -0600

    Add some styles to the card

--- src/components/productCard.vue
+++ src/components/productCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card flat tile style="padding-left:5px; padding-right: 5px">
+    <v-card flat tile style="border:1px solid #ccc;margin: 10px;">
         <v-img :src="product.imagelink" height="400px" class="black--text" style="text-align: center;background-color:#F0F0F0;">
             <h3 class="headline mb-0">{{ product.name }}</h3>
         </v-img>
